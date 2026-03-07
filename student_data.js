df5520c7a75a86ccf3e966097f31a81bb6a8d8ff
zel-92 <patrisjocel@gmail.com>
2026-03-07 19:56:33 +0800

    updated dashboard with drop down

---
+++ student_data.js
@@ -0,0 +1,22 @@
+{
+  "students": [
+    {
+      "name": "Tricia Mae Zamora",
+      "uid": "09999999",
+      "contact": "095608426",
+      "email": "hehfjdshsa@email.com",
+      "address": "123 Hotdog Kanto Street Caloocan",
+      "overallStats": {
+        "Attendance": 95,
+        "Late": 2,
+        "Undertime": 1,
+        "Absent": 2
+      },
+      "attendanceLogs": [
+        { "date": "2026-03-01", "status": "Present" },
+        { "date": "2026-03-05", "status": "Late" },
+        { "date": "2026-03-07", "status": "Late"}
+      ]
+    }
+  ]
+}
